09a3cead9bcad53a3a1f8da022b7817eadf39e9c
2286981662@qq.com <2286981662@qq.com>
2021-02-03 23:33:36 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Project LibraryManage init by author@Lee
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
